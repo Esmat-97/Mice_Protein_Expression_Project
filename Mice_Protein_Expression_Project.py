fe163b0a83a6e5e266e36c473d081bd4c60baef6
mohamedesmat1997 <158211063+63456356@users.noreply.github.com>
2026-02-25 15:23:48 +0200

    Initial commit

--- Mice_Protein_Expression_Project.py
+++ Mice_Protein_Expression_Project.py
@@ -56,11 +56,11 @@ plt.show()
 
 
 
-import plotly.express as px # Interactive scatter plot of protein expression across proteins and genotypes 
-fig = px.scatter(df, x="Genotype", y="DYRK1A_N", color="Treatment",  # Using DYRK1A_N for illustration 
-    title="Interactive Protein Expression Plot") 
-fig.write_html("interactive_expression_plot.html")  # Save as HTML 
-fig.show() 
+# import plotly.express as px # Interactive scatter plot of protein expression across proteins and genotypes 
+# fig = px.scatter(df, x="Genotype", y="DYRK1A_N", color="Treatment",  # Using DYRK1A_N for illustration 
+#     title="Interactive Protein Expression Plot") 
+# fig.write_html("interactive_expression_plot.html")  # Save as HTML 
+# fig.show() 
 
 
 
